e98dfab9842657382ed21cb03e024ecca88b31b5
TobiP <a15237@freenet.de>
2018-04-05 15:40:48 +0900

    Fixed adding else-branches to list

--- BranchCollector.py
+++ BranchCollector.py
@@ -10,11 +10,18 @@ def add_childs(node, branch_list, parents_list):
 	for child_node in ast.iter_child_nodes(node):
 		if check_instance(child_node):
 			tmp_list = parents_list[:]
-			# tmp_list.append(child_node.__class__.__name__)
-			tmp_list.append(child_node)
-			branch_list.append(tmp_list)
-			add_childs(child_node, branch_list, tmp_list)
+			if not child_node in node.body:
+				# node is part of orelse list
+				tmp_list[-1] = node.orelse[0].lineno - 1
+			tmp_list.append(child_node.lineno)
+			branch_list.append(tmp_list[:])
 
+			if hasattr(child_node, 'orelse'):
+				if child_node.orelse:
+					else_list = tmp_list[:]
+					else_list[-1] = child_node.orelse[0].lineno - 1
+ 					branch_list.append(else_list[:])
+			add_childs(child_node, branch_list, tmp_list)
 
 def check_instance(node):
 	"""
@@ -22,8 +29,8 @@ def check_instance(node):
 	"""
 	if (isinstance(node, ast.If)
 		or isinstance(node, ast.For) 
-		or isinstance(node, ast.While)
-		or isinstance(node, ast.FunctionDef)):
+		or isinstance(node, ast.While)):
+		# or isinstance(node, ast.FunctionDef)):
 		# relevant type
 		return True
 	else:
@@ -38,8 +45,9 @@ def collect_branches(ast_tree):
 	branch_list = []
 	for node in ast.iter_child_nodes(ast_tree):
 		if check_instance(node):
-			# branch_list.append([node.__class__.__name__])
-			# add_childs(node, branch_list, [node.__class__.__name__])
-			branch_list.append([node])
-			add_childs(node, branch_list, [node])
+			branch_list.append([node.lineno])
+			if hasattr(node, 'orelse'):
+				if node.orelse:
+ 					branch_list.append([node.orelse[0].lineno - 1])
+			add_childs(node, branch_list, [node.lineno])
 	return branch_list
