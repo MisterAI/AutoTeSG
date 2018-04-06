9ba234ea4c8b9772418df8d0b08e4d562c4b320f
TobiP <a15237@freenet.de>
2018-04-06 15:05:18 +0900

    Fixed missing print for else + fixed replacement of print stmts

--- CodeInstrumentator.py
+++ CodeInstrumentator.py
@@ -10,20 +10,22 @@ class RemovePrintStmts(NodeTransformer):
 	"""
 	def visit_Print(self, node):
 		self.generic_visit(node)
-		node = Expr(value=Tuple(elts=node.values, ctx=Load()))
-		fix_missing_locations(node)
-		return node
-		# return None
+		new_node = []
+		for i_node in node.values:
+			new_node.append(Expr(value=i_node, ctx=Load()))
+			fix_missing_locations(new_node[-1])
+		return new_node
 
 	def visit_Expr(self, node):
 		self.generic_visit(node)
 		if hasattr(node.value, 'func'):
 			if hasattr(node.value.func, 'id'):
 				if node.value.func.id == 'print':
-					new_expr = Expr(value=Tuple(elts=node.value.args, ctx=Load()))
-					node = new_expr
-					fix_missing_locations(node)
-					return node
+					new_node = []
+					for i_node in node.value.args:
+ 						new_node.append(Expr(value=i_node, ctx=Load()))
+ 						fix_missing_locations(new_node[-1])
+					return new_node
 		return node
 
 class CodeInstrumentator(NodeTransformer):
@@ -33,6 +35,11 @@ class CodeInstrumentator(NodeTransformer):
 	def insert_print(self, node):
 		node.body.insert(0, self.get_print_stmt(node.lineno))
 		fix_missing_locations(node.body[0])
+		if hasattr(node, 'orelse'):
+			if node.orelse:
+				node.orelse.insert(0, 
+					self.get_print_stmt(node.orelse[0].lineno - 1))
+				fix_missing_locations(node.orelse[0])
 
 	def visit_If(self, node):
 		self.insert_print(node)
