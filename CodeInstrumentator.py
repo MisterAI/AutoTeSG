461a0760e8093ffe49a60ca33ef9226c31c97f02
TobiP <a15237@freenet.de>
2018-04-08 11:31:13 +0900

    Added distance calculation to print statements while instrumenting code

--- CodeInstrumentator.py
+++ CodeInstrumentator.py
@@ -23,22 +23,160 @@ class RemovePrintStmts(NodeTransformer):
 				if node.value.func.id == 'print':
 					new_node = []
 					for i_node in node.value.args:
- 						new_node.append(Expr(value=i_node, ctx=Load()))
- 						fix_missing_locations(new_node[-1])
+						new_node.append(Expr(value=i_node, ctx=Load()))
+						fix_missing_locations(new_node[-1])
 					return new_node
 		return node
 
 class CodeInstrumentator(NodeTransformer):
-	def get_print_stmt(self, lineno):
-		return parse('print(' + str(lineno) + ')').body[0]
+	comparison_thresh = 0.01
+
+	def dist_Eq(self, left, right):
+		# distance = abs(left-right)
+		return Call(func=Name(id='abs'),
+					args=[BinOp(
+						left=left,
+						op=Sub(),
+						right=right)],
+					keywords=[],
+					starargs=None,
+					kwargs=None)
+	
+	def dist_NotEq(self, left, right):
+		# distance = -abs(left-right)
+		return UnaryOp(op=USub(),
+					operand=Call(
+						func=Name(id='abs'),
+						args=[BinOp(left=left, op=Sub(), right=right)],
+						keywords=[],
+						starargs=None,
+						kwargs=None)
+					)
+
+	def dist_Lt(self, left, right):
+		# distance = left-right + threshold
+		return BinOp(
+			left=BinOp(
+				left=left, 
+				op=Sub(), 
+				right=right), 
+			op=Add(), 
+			right=Num(n=self.comparison_thresh))
+
+	def dist_LtE(self, left, right):
+		# use same as Lt
+		return self.dist_Lt(left, right)
+
+	def dist_Gt(self, left, right):
+		# turn around args
+		return self.dist_Lt(right, left)
+
+	def dist_GtE(self, left, right):
+		# turn around args
+		return self.dist_LtE(right, left)
+
+	def dist_Is(self, left, right):
+		# distance cannot be quantified easily; return 1 for now 
+		return Num(n=1)
+
+	def dist_IsNot(self, left, right):
+		# distance cannot be quantified easily; return 1 for now 
+		return Num(n=1)
+
+	def dist_In(self, left, right):
+		# approximate distance by distance to nearest item in list
+		return Call(
+			func=Name(id='min'),
+			args=[
+				ListComp(
+					elt=Call(func=Name(id='abs'),
+						args=[BinOp(left=left, op=Sub(), right=Name(id='item'))],
+						keywords=[],
+						starargs=None,
+						kwargs=None),
+					generators=[comprehension(target=Name(id='item'), iter=right, ifs=[])])],
+			keywords=[],
+			starargs=None,
+			kwargs=None)
+
+	def dist_NotIn(self, left, right):
+		# distance is 1 for integer
+		# we only consider the case 'left not in right'
+		return Num(n=1)
+
+	def dist_functs(self, op):
+		dist_functs = {
+			'Eq': self.dist_Eq,
+			'NotEq': self.dist_NotEq,
+			'Lt': self.dist_Lt,
+			'LtE': self.dist_LtE,
+			'Gt': self.dist_Gt,
+			'GtE': self.dist_GtE,
+			'Is': self.dist_Is,
+			'IsNot': self.dist_IsNot,
+			'In': self.dist_In,
+			'NotIn': self.dist_NotIn
+		}
+		return dist_functs[op]
+
+	def calc_dist(self, node):
+		if isinstance(node, If) or isinstance(node, While):
+			if isinstance(node.test, Compare):
+				distance = None
+				for i in range(0, len(node.test.ops)):
+					# sum up distances of all comparisons
+					operation = astor.dump_tree(node.test.ops[0])
+					if i == 0:
+						distance = self.dist_functs(operation)(
+							node.test.left, 
+							node.test.comparators[0])
+					else:
+						distance = BinOp(
+							left=distance,
+							op=Add(),
+							right=self.dist_functs(operation)(
+								node.test.comparators[i-1], 
+								node.test.comparators[i])
+							)
+				return distance
+			else:
+				# no compare node; return 1 for now
+				# TODO consider simple integer variable
+				return Num(n=1)
+		else:
+			if isinstance(node, For):
+				# distance equivalent to 'if(a in b)'
+				return self.dist_In(node.target, node.iter)
+
+	def get_print_stmt(self, lineno, node, else_branch):
+		if else_branch:
+			print_node = parse('print('+ str(lineno) + ',' + 'tmp' 
+				+ ')').body[0]
+			# invert distance for else branch
+			try:
+				print_node.values[0].elts[1] = UnaryOp(
+					op=USub(), 
+					operand=self.calc_dist(node))
+			except Exception as e:
+				print('TODO!!!, CodeInstrumentator.py')
+				# raise e
+			return print_node
+		else:
+			print_node = parse('print(' + str(lineno) + ',' + 'tmp' + ')').body[0]
+			try:
+				print_node.values[0].elts[1] = self.calc_dist(node)
+			except Exception as e:
+				print('TODO!!!, CodeInstrumentator.py')
+				# raise e
+			return print_node
 
 	def insert_print(self, node):
-		node.body.insert(0, self.get_print_stmt(node.lineno))
+		node.body.insert(0, self.get_print_stmt(node.lineno, node, False))
 		fix_missing_locations(node.body[0])
 		if hasattr(node, 'orelse'):
 			if node.orelse:
 				node.orelse.insert(0, 
-					self.get_print_stmt(node.orelse[0].lineno - 1))
+					self.get_print_stmt(node.orelse[0].lineno - 1, node, True))
 				fix_missing_locations(node.orelse[0])
 
 	def visit_If(self, node):
