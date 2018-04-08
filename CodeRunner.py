a7304c6213187115d3db25424deff97579291607
TobiP <a15237@freenet.de>
2018-04-08 19:39:44 +0900

    Compile and execute seperatedly for easier fault localisation

--- CodeRunner.py
+++ CodeRunner.py
@@ -2,23 +2,31 @@
 
 import sys
 import ast
+import astor
 
 if sys.version_info[0] < 3:
 	from io import BytesIO
 else:
 	from io import StringIO
 
-def getCodeOutput(codeTree):
+def runCode(codeTree):
 	"""
-	Temporarily redirect output to own IO
+	Run code and return output, generated on stdout.
 	"""
+
+	# Temporarily redirect output to own IO
 	old_stdout = sys.stdout
 	if sys.version_info[0] < 3:
 		my_output = sys.stdout = BytesIO()
 	else:
 		my_output = sys.stdout = StringIO()
 
-	compiled_code = compile(codeTree, filename="<ast>", mode="exec")
+	# instru_source = astor.to_source(codeTree)
+	# source_file = open('test.py', 'w')
+	# source_file.write(instru_source)
+	# source_file.close()
+
+	compiled_code = compile(astor.to_source(codeTree), filename="<ast>", mode="exec")
 	exec(compiled_code)
 	
 	# Restore sys stdout
